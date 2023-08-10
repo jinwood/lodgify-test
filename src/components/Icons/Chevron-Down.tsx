6748e13a32fae51645e116b1b0c6155188b96c86
jinwood <jinw@protonmail.com>
2023-08-10 13:43:38 +0100

    Tweaks
    SVG size
    Border tweaks

--- src/components/Icons/Chevron-Down.tsx
+++ src/components/Icons/Chevron-Down.tsx
@@ -2,8 +2,8 @@ export default function ChevronDown() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width="10"
+      height="10"
       fill="currentColor"
       viewBox="0 0 16 16"
     >
